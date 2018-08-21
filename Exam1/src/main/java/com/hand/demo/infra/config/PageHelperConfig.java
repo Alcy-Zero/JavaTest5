6eaaab786dbbd351c9c0f29e888eeddf2f1c1bc4
Alcy-Zero <chenbin.lv@hand-china.com>
2018-08-21 10:57:20 +0800

    [ADD]08-21-a01-补充内容

--- Exam1/src/main/java/com/hand/demo/infra/config/PageHelperConfig.java
+++ Exam1/src/main/java/com/hand/demo/infra/config/PageHelperConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import java.util.Properties;
 
 @Configuration
-public class MybatisConfig {
+public class PageHelperConfig {
     @Bean
     public PageHelper pageHelper() {
 
